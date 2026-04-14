8d564b7c55f27c6c074bde76bfea9de58ab6aea6
kennyvel <kenneth.velasquez756@myci.csuci.edu>
2026-04-14 06:37:02 -0700

    Message Grouping
    Recent messages from the same person will omit the message header (sender name and timestamp). The allowed time gap between messages for grouping can be changed in the shouldGroup function.

--- Frontend-Service/src/components/Message.jsx
+++ Frontend-Service/src/components/Message.jsx
@@ -1,16 +1,20 @@
-export default function Message({ message }) {
+export default function Message({ message, hideHeader }) {
 	const time = new Date(message.id).toLocaleTimeString([], {
 		hour: "numeric",
 		minute: "2-digit",
 	});
 
 	return (
-		<div className="message">
-			<div className="message-header">
-				<strong>{message.sender}</strong>
-				<span className="time">{time}</span>
-			</div>
-			<p className="message-text">{message.text}</p>
+		<div className={`message ${hideHeader ? "grouped" : ""}`}>
+			{!hideHeader && (
+				<div className="message-header">
+					<strong>{message.sender}</strong>
+					<span className="time">{time}</span>
+				</div>
+			)}
+			<p className="message-text">
+				{message.text}
+			</p>
 		</div>
 	)
 }
--- Frontend-Service/src/components/MessageList.jsx
+++ Frontend-Service/src/components/MessageList.jsx
@@ -1,11 +1,30 @@
 import Message from "./Message";
 
 export default function MessageList({ messages }) {
+	function shouldGroup(prev, current) {
+		if (!prev) {
+			return false;
+		} 
+		const sameUser = prev.sender === current.sender;
+		// optional: time gap rule (set at .1 minutes or 6 seconds for in class demo)
+		const timeDiff =
+			current.id - prev.id < .1 * 60 * 1000;
+		return sameUser && timeDiff;
+	}
+
 	return (
 		<div className="messages">
-			{messages.map((msg) => (
-        		<Message key={msg.id} message={msg} />
-      		))}
+			{messages.map((msg, index) => {
+				const prev = messages[index - 1];
+				const grouped = shouldGroup(prev, msg);
+				return (
+					<Message 
+						key={msg.id} 
+						message={msg} 
+						hideHeader={grouped}
+					/>
+				);
+			})}
 		</div>
 	)
 }
