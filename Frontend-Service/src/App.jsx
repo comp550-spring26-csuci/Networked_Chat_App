6e670f2365e6c076cc587a4b3906806edca9d537
csuci-briellebrantner <brielle.brantner170@myci.csuci.edu>
2026-05-11 08:26:32 -0700

    changed react router to redirect to friendslist upon login for testing

--- Frontend-Service/src/App.jsx
+++ Frontend-Service/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
-import ChatWindow from "./components/ChatWindow";
 import FriendsList from "./components/FriendsList";
 
 export default function App() {
@@ -10,10 +9,23 @@ export default function App() {
 
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage onLogin={setUser} />} />
-      <Route path="/message" element={user ? <ChatWindow user={user} onLogout={() => setUser(null)} /> : <Navigate to="/login" />} />
-      <Route path="/friends" element={user ? <FriendsList currentUser={user} /> : <Navigate to="/login" />} />
-      <Route path="*" element={<Navigate to="/login" />} />
+      {/* If a user exists, redirect them away from the login page to /friends */}
+      <Route 
+        path="/login" 
+        element={user ? <Navigate to="/friends" replace /> : <LoginPage onLogin={setUser} />} 
+      />
+      
+      {/* if no user exists, redirect back to /login */}
+      <Route 
+        path="/friends" 
+        element={user ? <FriendsList currentUser={user} /> : <Navigate to="/login" replace />} 
+      />
+      
+      {/* sends to friends if logged in, otherwise to login */}
+      <Route 
+        path="*" 
+        element={<Navigate to={user ? "/friends" : "/login"} replace />} 
+      />
     </Routes>
   );
 }
