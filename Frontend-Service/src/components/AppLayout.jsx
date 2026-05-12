f19b2b3a9609241b5f354322733c1dee23a072d3
kennyvel <kenneth.velasquez756@myci.csuci.edu>
2026-05-12 15:36:39 -0700

    Notification badge
    Styles ad hard coded notification badge for the messaging icon in the sidebar.

--- Frontend-Service/src/components/AppLayout.jsx
+++ Frontend-Service/src/components/AppLayout.jsx
@@ -9,21 +9,23 @@ export default function AppLayout() {
 	return (
 		<div className="app-shell">
 			<div className="sidebar">
-				<button 
-					className={`sidebar-icon ${location.pathname === "/chat" ? "active" : ""}`}
-					onClick={() => navigate("/chat")}
-				>
-					<MdEmail size={30}/>
-				</button>
-
+				{/* currrently the friends route doesnt exist on this branch so this will redirect you to nothing */}
 				<button
   					className={`sidebar-icon ${location.pathname === "/friends" ? "active" : ""}`}
   					onClick={() => navigate("/friends")}
 				>
 					<FaUserFriends size={30}/>
 				</button>
-			</div>
 
+				<button 
+					className={`sidebar-icon ${location.pathname === "/chat" ? "active" : ""}`}
+					onClick={() => navigate("/chat")}
+				>
+					<MdEmail size={30}/>
+					<span className="badge">3</span>
+				</button>
+			</div>
+			{/* all the content to the sight of the sidebar is rendered in Outlet (Possible routes are nested in AppLayout in App.jsx */}
 			<div className="main-view">
 				<Outlet />
 			</div>
