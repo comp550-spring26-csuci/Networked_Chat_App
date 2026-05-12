9dc9f8e626ae87adde9fef0239db86bc8f95b6ad
kennyvel <kenneth.velasquez756@myci.csuci.edu>
2026-05-12 07:47:42 -0700

    React icons for sidebar
    Adding the react icons dependency to load in multiple different icons for the sidebar. The following link shows all the icons available and shows code for how to import and render each one: https://react-icons.github.io/react-icons/

--- Frontend-Service/src/components/AppLayout.jsx
+++ Frontend-Service/src/components/AppLayout.jsx
@@ -1,4 +1,6 @@
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { MdEmail } from "react-icons/md";
+import { FaUserFriends } from "react-icons/fa";
 
 export default function AppLayout() {
 	const navigate = useNavigate();
@@ -11,14 +13,14 @@ export default function AppLayout() {
 					className={`sidebar-icon ${location.pathname === "/chat" ? "active" : ""}`}
 					onClick={() => navigate("/chat")}
 				>
-					💬
+					<MdEmail size={30}/>
 				</button>
 
 				<button
   					className={`sidebar-icon ${location.pathname === "/friends" ? "active" : ""}`}
   					onClick={() => navigate("/friends")}
 				>
-					👥
+					<FaUserFriends size={30}/>
 				</button>
 			</div>
 
