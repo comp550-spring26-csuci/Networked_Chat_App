09415a11d482de1288ecd2b84df86647a465759e
kennyvel <kenneth.velasquez756@myci.csuci.edu>
2026-05-21 02:21:44 -0700

    Friends List Styling Fixes

--- Frontend-Service/src/App.jsx
+++ Frontend-Service/src/App.jsx
@@ -8,30 +8,6 @@ import ChatLayout from './components/ChatLayout';
 //import { startConnection } from './signalr/chatConnection';
 
 function App() {
-  // const handleLogin = async (name) => {
-  //   try {
-  //     console.log("Name:", name);
-  //     const res = await fetch(
-  //       `https://sslk8rt0-7081.usw3.devtunnels.ms/api/testdm/seed-user?UserName=${name}&OverWrite=false`, {
-  //       method: 'POST'
-  //     });
-
-  //     const data = await res.json();
-
-  //     console.log("Login response:", data);
-
-  //     // store token
-  //     localStorage.setItem("access_token", data.token);
-
-  //     setUsername(data.username);
-  //     setIsLoggedIn(true);
-  //     startConnection();
-
-  //   } catch (err) {
-  //     console.error("Login failed:", err);
-  //   }
-  // };
-
   return (
     <HashRouter>
       <Routes>
