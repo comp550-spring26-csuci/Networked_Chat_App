825a8cc8e4918bc93b2497c551a5e66a37b67cee
kennyvel <kenneth.velasquez756@myci.csuci.edu>
2026-05-20 21:51:42 -0700

    Update Message.jsx

--- Frontend-Service/src/components/Message.jsx
+++ Frontend-Service/src/components/Message.jsx
@@ -2,17 +2,38 @@ export default function Message({ message, hideHeader }) {
 	console.log("MSG in message:", message);
 	console.log(`MSG SENDER in message: ${message.message.senderUsername}`);
 
-	const time = new Date(message.message.timestamp).toLocaleTimeString([], {
+	const msgDate = new Date(message.Message.timestamp);
+	const timeNow = new Date();
+
+	const isToday = msgDate.toDateString() === timeNow.toDateString();
+
+	const isYesterday = new Date(timeNow.setDate(timeNow.getDate() - 1)).toDateString === msgDate.toDateString();
+
+	const timePart = msgDate.toLocaleTimeString([], {
 		hour: "numeric",
 		minute: "2-digit",
 	});
+
+	let formattedTime;
+
+	if (isToday) {
+		formattedTime = timePart;
+	} else if (isYesterday) {
+		formattedTime = `Yesterday at ${timePart}`;
+	} else {
+		formattedTime = `${msgDate.toLocaleDateString([], {
+			month: "numeric",
+			day: "numeric",
+			year: "2-digit",
+		})}, ${timePart}`;
+	}
 
 	return (
 		<div className={`message ${hideHeader ? "grouped" : ""}`}>
 			{!hideHeader && (
 				<div className="message-header">
 					<strong>{message.message.senderUsername}</strong>
-					<span className="time">{time}</span>
+					<span className="time">{formattedTime}</span>
 				</div>
 			)}
 			<p className="message-text">
